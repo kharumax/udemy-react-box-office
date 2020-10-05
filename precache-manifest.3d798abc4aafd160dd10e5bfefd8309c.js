055fd3d9468f0f9bbb536339dc1731cd1d0c4208
kharumax <kharumax@gmail.com>
2020-10-05 16:53:45 +0900

    Updates

---
+++ precache-manifest.3d798abc4aafd160dd10e5bfefd8309c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea058daf2e3585c083f33567cd2ea0b",
+    "url": "/udemy-react-box-office/index.html"
+  },
+  {
+    "revision": "3b6622c208c01f428e39",
+    "url": "/udemy-react-box-office/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "f1c9d8947f14dd3cd398",
+    "url": "/udemy-react-box-office/static/js/2.605b082c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/udemy-react-box-office/static/js/2.605b082c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6622c208c01f428e39",
+    "url": "/udemy-react-box-office/static/js/main.93be34ce.chunk.js"
+  },
+  {
+    "revision": "4131e8e700c0b7e38c59",
+    "url": "/udemy-react-box-office/static/js/runtime-main.01693ed6.js"
+  }
+]);
